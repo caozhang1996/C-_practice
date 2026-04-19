81f230bc17483b7f695d6592948da59b079a1a15
caozhang <zhang.cao@ubtrobot.com>
2026-04-19 15:50:36 +0800

    fix: std::enable_if 补充说明

--- template_practice/include/template_practice/template_enable_if.h
+++ template_practice/include/template_practice/template_enable_if.h
@@ -12,16 +12,40 @@
 
 namespace template_enable_if_test
 {
-  // 要求函数的传参必须是 int 类型的
+  // 要求函数的传参必须是 int 类型的， 在该示例中没有给 std::enable_if_t
+  // 传入第二个参数，则 type 的默认类型为 void
   template <typename T,
             typename type = std::enable_if_t<std::is_same_v<T, int>>>
   void foo(T)
   {
   }
 
+  // 第二种写法，给 std::enable_if_t 传入第二个参数
+  // 此时 int = 0 (无名参数)
+  template <typename T, std::enable_if_t<std::is_same_v<T, double>, int> = 0>
+  void boo(T)
+  {
+  }
+
+  // bool = true (无名参数)
+  template <typename T,
+            std::enable_if_t<std::is_same_v<T, double>, bool> = true>
+  void boo_1(T)
+  {
+  }
+
+  // int* = nullptr (无名参数)
+  template <typename T,
+            std::enable_if_t<std::is_same_v<T, double>, int*> = nullptr>
+  void boo_2(T)
+  {
+  }
+
   void test()
   {
     foo(1);
+
+    boo(1.0);
 
     // 编译失败
     // foo(1.0);
